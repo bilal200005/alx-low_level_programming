f64266f03fdc1d375918c776e31d48af4ab61070
bilal200005 <bilalabkadir7@gmail.com>
2022-06-30 04:49:35 -0700

    added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - will print out put using prinf function
+ *Description : I used print f function with new line
+ *Return: 0 it will return 0 if the program successed
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
