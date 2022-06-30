db28b4f6c88386cc0488e965bd05964acc5f6b91
bilal200005 <bilalabkadir7@gmail.com>
2022-06-30 04:43:12 -0700

    added 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - will print the ouput in the print function
+ *
+ *Description : we will use puts functions for this task
+ *and it will print the text in the puts function
+ *Return: 0 it will return 0 if the program successed
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
