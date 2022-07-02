ba6ef69cd4dbd9ff93343af364c3820afb976e53
bilal200005 <bilalabkadir7@gmail.com>
2022-07-02 02:25:35 -0700

    added 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entyr point
+ * Description: prints two digits combination
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int n;
+int m;
+for (n = '0' ; n <= '9'; n++)
+{
+for (m = 0 ; m <= '9'; m++)
+{
+if (n < m)
+{
+putchar(n);
+putchar(m);
+if (n == '8' && m == '9')
+{
+}
+else
+{
+putchar(',');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
