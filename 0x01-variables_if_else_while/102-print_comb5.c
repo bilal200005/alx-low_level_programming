569f5c77fae3e821f601f72654d7b92be3da8f57
bilal200005 <bilalabkadir7@gmail.com>
2022-07-02 07:19:30 -0700

    added 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entyr point
+ * Description: prints 4 digits combination
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i, j, k, l;
+for (i = '0'; i <= '9'; i++)
+{
+for (j = '0'; j <= '9'; j++)
+{
+for (k = '0'; k <= '9'; k++)
+{
+for (l = '0'; l <= '9'; l++)
+{
+if((k + l) > (i+j))
+{
+putchar(i);
+putchar(j);
+putchar(' ');
+putchar(k);
+putchar(l);
+putchar(',');
+}
+}
+}
+}
+}
+return (0);
+}
