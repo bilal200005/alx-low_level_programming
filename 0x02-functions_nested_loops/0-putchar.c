f164a6629ec08eaea62c69bb591708f8f1c01976
bilal200005 <bilalabkadir7@gmail.com>
2022-07-05 11:35:30 -0700

    fixed the first bug

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- *main start the code
+ *main - Enter code
  *Discription: i will use putchar function to print
  *_putchar followed by new line
  *Return:0 this program will return zero if successed
  */
 int main(void)
 {
-char c[9] = "_putchar";
+char c[8] = "_putchar";
 int i;
-for (i = 0; i < 10; i++)
+for (i = 0; i < 9; i++)
 {
 _putchar(c[i]);
 }
