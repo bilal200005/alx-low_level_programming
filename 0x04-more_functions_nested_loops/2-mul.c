010ab7b08748a63a29234bd225d79cb54f8046ef
bilal200005 <bilalabkadir7@gmail.com>
2022-07-07 14:32:08 -0700

    added 2-mul and updated main.h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*mul - Multiply 2 integers.
+*@a: first argument
+*@b: 2nd argument
+*Return: sum returns the sum of the num
+*/
+int mul(int a, int b)
+{
+int sum;
+sum = a * b;
+return (sum);
+}
