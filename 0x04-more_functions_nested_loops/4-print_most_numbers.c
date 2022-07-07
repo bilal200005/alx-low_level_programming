5c52808efb75639b065e36242ddce6ea797bf4c7
bilal200005 <bilalabkadir7@gmail.com>
2022-07-07 15:03:57 -0700

    added 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_most_numbers - the fucntion pro
+*/
+void print_most_numbers(void)
+{
+int i = '0';
+while (i <= '9')
+{
+if (i != '2' && i != '4')
+{
+_putchar(i);
+}
+i++;
+}
+_putchar('\n');
+}
