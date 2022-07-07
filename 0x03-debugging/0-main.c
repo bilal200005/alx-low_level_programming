3438b54f7dfd47147434bfdee02332d083e6ce44
bilal200005 <bilalabkadir7@gmail.com>
2022-07-07 05:52:45 -0700

    fixed 0-main.c betty coding error

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,16 @@
 #include "main.h"
+
 /**
  *main - start the code
  *Return:0 return 0 if successed
  */
+
 int main(void)
 {
-int i = 0;
+int i;
+i = 0;
+
 positive_or_negative(i);
+
 return (0);
 }
