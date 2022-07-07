7efb5e071c7680d8a1199e135264b136b4fd2e9a
bilal200005 <bilalabkadir7@gmail.com>
2022-07-07 14:42:52 -0700

    added 3-print_numbers.c and updated main.h

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_numbers - the fucntion pro
+*/
+void print_numbers(void)
+{
+    int i='0';
+    while (i <= '9')
+    {
+        _putchar(i);
+        i++;
+    }
+    _putchar('\n');
+}
