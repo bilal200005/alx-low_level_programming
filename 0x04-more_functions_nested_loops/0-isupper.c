2c873fc937a16e7584e2a1bc9be34589d4959f80
bilal200005 <bilalabkadir7@gmail.com>
2022-07-07 14:09:17 -0700

    added README.md 0-isupper.c main.h

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_isupper - check if capital
+*@c: argument
+*Return:1 if capita;l
+*/
+int _isupper(int c)
+{
+if (c > 64 && c < 91)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
